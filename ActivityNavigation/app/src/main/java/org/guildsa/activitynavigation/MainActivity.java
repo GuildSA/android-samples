2646c416b10a7389514b9ddb1b69b428eb87a867
Kevin Harris <kharris1@fossil.com>
2016-03-10 12:36:28 -0600

    Add ActivityNavigation sample

---
+++ ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/MainActivity.java
@@ -0,0 +1,85 @@
+package org.guildsa.activitynavigation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText userNameEdit;
+    private EditText passswordEdit;
+
+    private TextView screenNameText;
+    private TextView highScoreText;
+
+    private final int MY_CHILD_ACTIVITY = 1;
+
+    @Override
+    public void onCreate( Bundle savedInstanceState ) {
+
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        userNameEdit = (EditText) findViewById( R.id.user_name );
+        passswordEdit = (EditText) findViewById( R.id.password );
+        Button btnNextScreen = (Button) findViewById( R.id.next_screen );
+        screenNameText = (TextView) findViewById( R.id.screen_name );
+        highScoreText = (TextView) findViewById( R.id.high_score );
+
+        // Listening to button event
+        btnNextScreen.setOnClickListener( new View.OnClickListener() {
+
+            public void onClick( View arg0 ) {
+
+                // Starting a new Intent
+                Intent nextScreen = new Intent( getApplicationContext(), SecondActivity.class );
+
+                // Sending data to another Activity
+                nextScreen.putExtra( "userName", userNameEdit.getText().toString() );
+                nextScreen.putExtra( "password", passswordEdit.getText().toString() );
+
+                // Start a new activity and ignore any results.
+                // startActivity( nextScreen );
+
+                // Start a new activity and catch the results.
+                startActivityForResult( nextScreen, MY_CHILD_ACTIVITY );
+            }
+        } );
+    }
+
+    @Override
+    public void onActivityResult( int requestCode, int resultCode, Intent data ) {
+
+        super.onActivityResult( requestCode, resultCode, data );
+
+        switch( requestCode ) {
+
+            case MY_CHILD_ACTIVITY:
+
+                if( resultCode == Activity.RESULT_OK ) {
+
+                    Log.d(TAG, "Activity.RESULT_OK");
+
+                    // Get the data passed to us.
+                    String screenName = data.getStringExtra( "screenName" );
+                    int highScore = data.getIntExtra( "highScore", 0 );
+
+                    Log.d( TAG, "screenName = " + screenName );
+                    Log.d( TAG, "highScore = " + String.valueOf( highScore ) );
+
+                    screenNameText.append( screenName );
+                    highScoreText.append( String.valueOf( highScore ) );
+
+                break;
+            }
+        }
+    }
+}
---
+++ ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/SecondActivity.java
@@ -0,0 +1,54 @@
+package org.guildsa.activitynavigation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_second );
+
+		TextView userNameText = (TextView) findViewById( R.id.user_name );
+		TextView passwordText = (TextView) findViewById( R.id.password );
+		Button closeBtn = (Button) findViewById( R.id.close );
+
+		Intent intent = getIntent();
+
+		// Get the data passed to us.
+		String userName = intent.getStringExtra( "userName" );
+		String password = intent.getStringExtra( "password" );
+
+		// Displaying received data.
+		userNameText.setText( userName );
+		passwordText.setText( password );
+
+		// Binding Click event to Button
+		closeBtn.setOnClickListener( new View.OnClickListener() {
+
+			public void onClick( View arg0 ) {
+
+				// Package up some data and set a result to send back 
+				// to the Activity that called us and close this Activity.
+				
+				Intent resultIntent = new Intent();
+				
+				// Let's pretend that we logged into the player's account and 
+				// got their screen name and current high score.
+				resultIntent.putExtra( "screenName", "c1pher" );
+				resultIntent.putExtra( "highScore", 12456 );
+				
+				setResult( Activity.RESULT_OK, resultIntent );
+
+				finish();
+			}
+		} );
+	}
+}
