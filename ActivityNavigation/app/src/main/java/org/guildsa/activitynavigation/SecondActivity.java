6d626c460ecddaec630ecc0f4849313409cdca20
Kevin Harris <kharris1@fossil.com>
2016-03-16 09:45:31 -0500

    Switch from Activity to AppCompatActivity for consistency.

--- ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/SecondActivity.java
+++ ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/SecondActivity.java
@@ -3,11 +3,12 @@ package org.guildsa.activitynavigation;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class SecondActivity extends Activity {
+public class SecondActivity extends AppCompatActivity {
 
 	/** Called when the activity is first created. */
 	@Override
