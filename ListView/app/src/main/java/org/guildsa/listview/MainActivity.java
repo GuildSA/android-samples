8b9da80656ec9f6415707a0d0a2b55ce55538f69
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-11-21 14:36:18 -0600

    Update.

--- ListView/app/src/main/java/org/guildsa/listview/MainActivity.java
+++ ListView/app/src/main/java/org/guildsa/listview/MainActivity.java
@@ -1,12 +1,13 @@
 package org.guildsa.listview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
