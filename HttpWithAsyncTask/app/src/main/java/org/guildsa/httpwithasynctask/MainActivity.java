f3e2962b7239a696a635921882e527ff1485d85d
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-11-21 14:23:17 -0600

    Update.

--- HttpWithAsyncTask/app/src/main/java/org/guildsa/httpwithasynctask/MainActivity.java
+++ HttpWithAsyncTask/app/src/main/java/org/guildsa/httpwithasynctask/MainActivity.java
@@ -1,7 +1,6 @@
 package org.guildsa.httpwithasynctask;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +13,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
