4d089286c11dde5bb924f7b6bc153319b225d6a2
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-11-21 12:57:12 -0600

    Update.

--- ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/MainActivity.java
+++ ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/MainActivity.java
@@ -2,13 +2,14 @@ package org.guildsa.activitynavigation;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
--- ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/SecondActivity.java
+++ ActivityNavigation/app/src/main/java/org/guildsa/activitynavigation/SecondActivity.java
@@ -3,10 +3,11 @@ package org.guildsa.activitynavigation;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
