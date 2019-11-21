3a5f76caaabc87fcb9e67cad41ff982d6f068820
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-11-21 14:29:52 -0600

    Update.

--- GUIEvents/app/src/main/java/org/guildsa/guievents/MainActivity.java
+++ GUIEvents/app/src/main/java/org/guildsa/guievents/MainActivity.java
@@ -2,7 +2,6 @@ package org.guildsa.guievents;
 
 import android.app.AlertDialog;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +10,8 @@ import android.widget.TextView;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
