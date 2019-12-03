4c2625865431aec131f6e8d957e7c66d71fddf22
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-12-03 14:25:16 -0600

    Update.

--- CustomArrayAdapter/app/src/main/java/org/guildsa/customarrayadapter/MainActivity.java
+++ CustomArrayAdapter/app/src/main/java/org/guildsa/customarrayadapter/MainActivity.java
@@ -1,12 +1,13 @@
 package org.guildsa.customarrayadapter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
