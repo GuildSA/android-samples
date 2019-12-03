79e2ecd13c5eaccc25c503b405e72719adbe44b3
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-12-03 13:26:22 -0600

    Update.

--- SharedPreferences/app/src/main/java/com/guildsa/sharedpreferences/MainActivity.java
+++ SharedPreferences/app/src/main/java/com/guildsa/sharedpreferences/MainActivity.java
@@ -2,12 +2,13 @@ package com.guildsa.sharedpreferences;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
