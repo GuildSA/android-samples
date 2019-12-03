4925ebf4da451dcceafae204ca5dacaf992a1851
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-12-03 14:18:03 -0600

    Update.

--- RecyclerView/app/src/main/java/org/guildsa/recyclerview/MyAdapter.java
+++ RecyclerView/app/src/main/java/org/guildsa/recyclerview/MyAdapter.java
@@ -1,10 +1,11 @@
 package org.guildsa.recyclerview;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
