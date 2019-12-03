c4bd1894f872e95bf6b4f2d0d673d19188ba1420
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-12-03 13:51:15 -0600

    Update.

--- JsonParsing/app/src/main/java/org/guildsa/jsonparsing/MainActivity.java
+++ JsonParsing/app/src/main/java/org/guildsa/jsonparsing/MainActivity.java
@@ -1,7 +1,5 @@
 package org.guildsa.jsonparsing;
 
-import android.support.annotation.RawRes;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,6 +11,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
+import androidx.annotation.RawRes;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
